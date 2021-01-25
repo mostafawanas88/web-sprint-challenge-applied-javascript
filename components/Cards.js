ec9b0acd137162dc1d51316f987cbfce43462cb1
mostafawanas88 <mostafawanas@hotmail.com>
2021-01-25 09:15:31 +0400

    submission no stretch

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,49 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+const articleContainer = document.querySelector('.cards-container');
+
+const articleMaker = function(artObj) {
+    // creating the elements
+    const cardDiv = document.createElement('div');
+    const headlineDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const imageDiv = document.createElement('div');
+    const authorImg = document.createElement('img');
+    const spanName = document.createElement('span');
+
+    // adding classes to elements
+    cardDiv.classList.add('card');
+    headlineDiv.classList.add('headline');
+    authorDiv.classList.add('author');
+    imageDiv.classList.add('img-container');
+
+    // adding data to elements
+    headlineDiv.textContent = artObj.headline;
+    authorImg.setAttribute('src', artObj.authorPhoto);
+    spanName.textContent = artObj.authorName;
+
+    // appending elements
+    cardDiv.appendChild(headlineDiv);
+    cardDiv.appendChild(authorDiv);
+    authorDiv.appendChild(imageDiv);
+    authorDiv.appendChild(spanName);
+    imageDiv.appendChild(authorImg);
+
+    // return main div
+    return cardDiv;
+}
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then((response) => {
+    for (var key in response.data.articles) {
+        response.data.articles[key].forEach((articleObj) => {
+            const newArticle = articleMaker(articleObj);
+            articleContainer.appendChild(newArticle);
+        })
+    }
+})
+.catch(err => console.log(err, 'error, no data found!'));
+
--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,34 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+// creating the elements
+    const headerDiv = document.createElement('div');
+    const spanElement1 = document.createElement('span');
+    const titleElement = document.createElement('h1');
+    const spanElement2 = document.createElement('span');
+
+// adding classes to elements
+    headerDiv.classList.add('header');
+    spanElement1.classList.add('date');
+    spanElement2.classList.add('temp');
+
+// adding text content to elements
+    spanElement1.textcontent = 'MARCH 28, 2020';
+    titleElement.textContent = 'Lambda Times';
+    spanElement2.textContent = '98';
+
+// appneding elements to each other 
+    headerDiv.appendChild(spanElement1);
+    headerDiv.appendChild(titleElement);
+    headerDiv.appendChild(spanElement2);
+
+// return headerDiv
+    return headerDiv;
+}
+
+const newsPaperHeader = Header();
+
+const mainHeader = document.querySelector('.header-container');
+
+mainHeader.appendChild(newsPaperHeader);
